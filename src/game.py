9d78dca7246ee3282e408434dcf9adcc7597caba
Morgan Gautherot <morgangautherot@Morgan.local>
2024-03-18 21:41:55 +0100

    docs(FBGM): add docstring to functions

--- src/game.py
+++ src/game.py
@@ -4,15 +4,14 @@ from src.windows import Background
 
 class Game:
     def __init__(self)->None:
-        """ Initialization of the game class"""
+        """Initialization of the game class"""
         pygame.init()
         pygame.display.set_caption("Flappy Bird")
         self.screen = pygame.display.set_mode((config.WIDTH, config.HEIGHT))
         self.background = Background()
 
     def start(self):
+        """Function that start the game"""
         
         while True: 
             self.background.draw(self.screen)
-
-
--- src/windows.py
+++ src/windows.py
@@ -2,13 +2,15 @@ import pygame
 import src.config as config
 
 class Background:
-    def __init__(self)->None:
+    def __init__(self) -> None:
         """Initialization og the background class"""
         self.image = pygame.image.load(config.BACKGROUND).convert()
 
     @property
     def rect(self) -> pygame.Rect:
+        """Store background coordonate"""
         return pygame.Rect(0, 0, config.WIDTH, config.HEIGHT)
 
     def draw(self, screen) -> None:
+        """Draw the background of the game"""
         screen.blit(self.image, self.rect)
