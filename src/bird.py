721d7d6dc9e33cac15c469c82b58d8965f6a2f8d
Morgan Gautherot <morgangautherot@Morgan.local>
2024-03-19 09:55:59 +0100

    refactor(FBGM): change for more understanable name and complete docstring

--- src/bird.py
+++ src/bird.py
@@ -7,6 +7,7 @@ from src.utils import clamp
 class Bird:
     def __init__(self) -> None:
         """Initialization of the class"""
+
         self.image = pygame.image.load(config.BIRD).convert()
         self.x = int(config.SCREEN_WIDTH * 0.2)
         self.y = int((config.SCREEN_HEIGHT - self.image.get_height()) / 2)
@@ -16,42 +17,66 @@ class Bird:
         self.min_y = -2 * self.h
         self.max_y = config.SCREEN_HEIGHT - self.h * 0.75
 
-        self.vel_y = -9  # bird velocity along Y axis
-        self.max_vel_y = 10  # max vel along Y, max descend speed
-        self.min_vel_y = -8  # min vel along Y, max ascend speed
-        self.acc_y = 1  # bird downward acceleration
+        self.velocity_y = -9
+        self.maximum_velocity_y = 10
+        self.minimum_velocity_y = -8
+        self.downward_acceleration_y = 1
 
-        self.rot = 80  # bird current rotation
-        self.vel_rot = -3  # bird rotation speed
-        self.rot_min = -90  # bird min rotation angle
-        self.rot_max = 20  # bird max rotation angle
+        self.current_rotation = 80
+        self.velocity_rotation = -3
+        self.rotation_minimum = -90
+        self.rotation_maximum = 20
 
-        self.flap_acc = -9  # bird speed on flapping
-        self.flapped = False  # True when player flaps
+        self.flap_acceleration = -9
+        self.bird_has_flapped = False
 
     @property
     def rect(self) -> pygame.Rect:
-        """Store background coordonates"""
+        """Store bird coordonates
+
+        Return:
+           pygame.Rect: return the pygame object containing the coordonate of the bird
+        """
         return pygame.Rect(self.x, self.y, self.w, self.h)
 
     def rotate(self) -> None:
-        """change rotation of the bird"""
-        self.rot = clamp(self.rot + self.vel_rot, self.rot_min, self.rot_max)
+        """Change rotation of the bird"""
+        self.current_rotation = clamp(
+            self.current_rotation + self.velocity_rotation,
+            self.rotation_minimum,
+            self.rotation_maximum,
+        )
 
     def next_statuts(self, screen: pygame.Surface) -> None:
-        """compute the next bird status"""
-        if self.vel_y < self.max_vel_y and not self.flapped:
-            self.vel_y += self.acc_y
-        if self.flapped:
-            self.flapped = False
+        """Compute the next bird status
 
-        self.y = clamp(self.y + self.vel_y, self.min_y, self.max_y)
+        Args:
+            screen(pygame.Surface): object contaning the information of the screen of the game
+        """
+        if self.velocity_y < self.maximum_velocity_y and not self.bird_has_flapped:
+            self.velocity_y += self.downward_acceleration_y
+        if self.bird_has_flapped:
+            self.bird_has_flapped = False
+
+        self.y = clamp(self.y + self.velocity_y, self.min_y, self.max_y)
         self.rotate()
 
         self.draw(screen)
 
-    def draw(self, screen) -> None:
-        """Draw the bird of the game"""
-        rotated_image = pygame.transform.rotate(self.image, self.rot)
+    def draw(self, screen: pygame.Surface) -> None:
+        """Draw the bird of the game
+
+        Args:
+            screen(pygame.Surface): object contaning the information of the screen of the game
+        """
+        rotated_image = pygame.transform.rotate(self.image, self.current_rotation)
         rotated_rect = rotated_image.get_rect(center=self.rect.center)
         screen.blit(rotated_image, rotated_rect)
+
+    def flap(self) -> None:
+        """Changes the bird's coordinates for a wingbeat if the bird is still
+        visible on the screen"""
+        if self.y > self.min_y:
+            self.velocity_y = self.flap_acceleration
+            self.bird_has_flapped = True
+            self.current_rotation = 80
--- src/game.py
+++ src/game.py
@@ -1,7 +1,7 @@
 import sys
 
 import pygame
-from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
+from pygame.locals import K_ESCAPE, K_SPACE, K_UP, KEYDOWN, QUIT
 
 import src.config as config
 from src.bird import Bird
@@ -26,13 +26,35 @@ class Game:
         while True:
             for event in pygame.event.get():
                 self.check_quit_event(event)
+                if self.is_tap_event(event):
+                    self.bird.flap()
             self.background.draw(self.screen)
             self.bird.next_statuts(self.screen)
             pygame.display.update()
             self.clock.tick(config.FPS)
 
     def check_quit_event(self, event: pygame.event) -> None:
-        """Function that enable the user to quit the game"""
+        """Function that enable the user to quit the game
+
+        Args:
+           event(pygame.event): the event of the user
+        """
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
+
+    def is_tap_event(self, event: pygame.event) -> bool:
+        """Return True if there is an event
+
+        Args:
+           event(pygame.event): the event of the user
+
+        Return:
+           bool: ture if the event is a space, up or click left
+        """
+        m_left, _, _ = pygame.mouse.get_pressed()
+        space_or_up = event.type == KEYDOWN and (
+            event.key == K_SPACE or event.key == K_UP
+        )
+        screen_tap = event.type == pygame.FINGERDOWN
+        return m_left or space_or_up or screen_tap
